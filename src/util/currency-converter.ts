321dc3cdc7ce8c5d100df8972c95bd9bde038adf
AndriyDubovichWork <andriydubovichwork@gmail.com>
2022-08-04 16:35:00 +0300

    add

---
+++ src/util/currency-converter.ts
@@ -0,0 +1,23 @@
+const rates: Record<string, number> = {
+  USD: 1.205736,
+  GBP: 0.866671,
+  AUD: 1.555409,
+  JPY: 130.210363,
+  UAH: 37.212112,
+  PLN: 4.78333,
+};
+
+const symbols = ['$', '£', 'A$', '¥', '₴', 'zł'];
+
+const currencyConverter = {
+  availableCurrencies: Object.keys(rates).map((label, i) => ({
+    label,
+    symbol: symbols[i],
+  })),
+
+  convertFromEUR(amount: number, targetCurrency: string) {
+    return rates[targetCurrency] * amount;
+  },
+};
+
+export default currencyConverter;
